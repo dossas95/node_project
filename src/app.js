12c3a018379bace6d1125ad04ed38242eab2407f
David Ossa <dossas@unal.edu.co>
2019-03-31 23:35:04 -0500

    Regristrar usuario nuevo

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 const path = require('path');
 var app = express();
 const {sesion} = require('./sesion');
+const bodyParser = require('body-parser')
 
 const dirNode_modules = path.join(__dirname , '../node_modules')
 
@@ -10,9 +11,11 @@ app.use('/js', express.static(dirNode_modules + '/jquery/dist'));
 app.use('/js', express.static(dirNode_modules + '/popper.js/dist'));
 
 app.use('/js', express.static(dirNode_modules + '/bootstrap/dist/js'));
+app.use(bodyParser.urlencoded({extended: false}))
 
 // Helpers
 require('./index_helper');
+require('./registrar_helper');
 
 const directorioPublico = path.join(__dirname, '../public');
 app.use(express.static(directorioPublico));
@@ -27,6 +30,21 @@ app.get('/', (req,res) => {
 app.get('/login', (req,res) => {
   res.render('login');
 });
+
+app.get('/registro', (req,res) => {
+  res.render('registro');
+});
+
+app.get('/registrar', (req,res) => {
+  res.render('registrar', {
+    usuario: req.query.nombre,
+    cedula: req.query.id,
+    correo: req.query.correo,
+    telefono: req.query.telefono,
+    contrasena: req.query.contrasena
+  })
+})
+
 
 app.use((req, res, next)=>{
   if(!sesion){
